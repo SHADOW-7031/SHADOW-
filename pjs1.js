545538632169e9446c34283a31bcced258125584
SHADOW-7031 <bhaminianjanasai2004@gmail.com>
2024-11-27 13:00:08 +0530

    Add files via upload

---
+++ pjs1.js
@@ -0,0 +1,38 @@
+var a=4;
+var b=2;
+if(a>b){
+    console.log("a is greater than b");
+}else{
+    console.log("a is not greater than b");
+};
+
+
+
+var a=4;
+var b=3;
+if(a<b){
+    console.log("a is less than b");
+}
+else{
+    console.log("b is less than a");
+}
+
+
+let mark = 66;
+if (mark >= 80) {
+    document.write("grade A");
+}else if(mark >= 60){
+    document.write("grade b");
+}else{ 
+    document.write("grade c")
+}
+
+for (var i = 0; i<=10; i++) {
+    console.log(i);
+    
+}
+
+for (let i = 1,j =65; i<100 & j<91; i++,j++) {
+    console.log(i,String.fromCharCode(j));
+    
+}
